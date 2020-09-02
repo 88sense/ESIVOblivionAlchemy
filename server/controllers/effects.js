ab6ebbbd670d3bb88afffd70807ac1c4a65f1032
Shannon Lee <shannon.lee@88sense.com>
2020-09-02 00:10:07 -0400

    created controller files

---
+++ server/controllers/effects.js
@@ -0,0 +1,21 @@
+import Effect from '../models/Effect';
+
+const effectController = {
+    index: function(req, res) {
+
+    },
+    create: function(req, res) {
+
+    },
+    read: function(req, res) {
+
+    },
+    update: function(req, res) {
+
+    },
+    delete: function(req, res) {
+
+    }
+}
+
+module.exports = effectController;
---
+++ server/controllers/ingredients.js
@@ -0,0 +1,21 @@
+import Ingredient from '../models/Ingredient';
+
+const ingredientController = {
+    index: function(req, res) {
+
+    },
+    create: function(req, res) {
+
+    },
+    read: function(req, res) {
+
+    },
+    update: function(req, res) {
+
+    },
+    delete: function(req, res) {
+
+    }
+}
+
+module.exports = ingredientController;
